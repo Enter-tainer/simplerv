401bfde1ae8889e1a1a70c8d95931d0c80fc957d
mgt <mgt@oi-wiki.org>
2022-03-12 02:47:52 +0800

    style: comment test code

--- ram.v
+++ ram.v
@@ -20,15 +20,15 @@ module ram (input clk,
   assign actual_addressp2[addr_width-1:0] = {actual_address[addr_width-1:2], 2'b10};
   assign actual_addressp3[addr_width-1:0] = {actual_address[addr_width-1:2], 2'b11};
   reg [7:0]mem[mem_size - 1:0];
-  wire [7:0]data0, data1, data2, data3, data4, data5, data6, data7;
-  assign data0 = mem[0];
-  assign data1 = mem[1];
-  assign data2 = mem[2];
-  assign data3 = mem[3];
-  assign data4 = mem[4];
-  assign data5 = mem[5];
-  assign data6 = mem[6];
-  assign data7 = mem[7];
+  // wire [7:0]data0, data1, data2, data3, data4, data5, data6, data7;
+  // assign data0 = mem[0];
+  // assign data1 = mem[1];
+  // assign data2 = mem[2];
+  // assign data3 = mem[3];
+  // assign data4 = mem[4];
+  // assign data5 = mem[5];
+  // assign data6 = mem[6];
+  // assign data7 = mem[7];
   integer i;
   always@(posedge clk) begin
     if (rst == 1) begin
