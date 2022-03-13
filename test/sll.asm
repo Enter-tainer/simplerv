93ca63ce7e47f0360bdf504bccfec18ab4605cc6
woolen-sheep <2460563632@qq.com>
2022-03-14 00:03:42 +0800

    test: add ccab test hex

--- test/sll.asm
+++ test/sll.asm
@@ -1,26 +1,28 @@
+#sll   revise date:2018/3/12 tiger
+#output 0x00000876 0x00008760 0x00087600 0x00876000 0x08760000 0x87600000 0x76000000 0x60000000 0x00000000
 
 .text
-addi s0,zero,1     #简单移位，循环测试，0号区域显示的是初始值1左移1位重复15次的值，1号区域是累加值
-addi s1,zero,1  
-slli s1, s1, 31   #逻辑左移31位 s1=0x80000000
- 
 
-###################################################################
-#                逻辑左移测试 
-# 显示区域依次显示0x80000000 0x20000000 0x08000000 0x02000000 0x00800000 0x00200000 0x00080000 0x00020000 0x00008000 0x00002000 0x00000800 0x00000200 0x00000080 0x00000020 0x00000008 0x00000002 0x00000000  
-###################################################################
-LogicalRightShift:            #逻辑右移测试，将最高位1逐位向右右移直至结果为零
+addi t0,zero,1     
+addi t1,zero,3     
+addi s1,zero,  0x8
+slli s1,s1,8
+addi s1,s1,0x76     
 
-add    a0,zero,s1       #display s1
-addi   a7,zero,34        # display hex
-# ecall                 # we are out of here.  
-     
-srli s1, s1, 2   
-beq s1, zero, shift_next1
-j LogicalRightShift
+add a0,zero,s1           
+addi a7,zero,34         # system call for print
+#ecall                  # print
 
-shift_next1:
+addi t3,zero,8
 
-add    a0,zero,s1       #display s1
-addi   a7,zero,34         # display hex
-# ecall                 # we are out of here.  
+sll_branch:
+sll s1,s1,t0       #1
+sll s1,s1,t1       #test
+add a0,zero,s1          
+addi a7,zero,34         # system call for print
+#ecall                  # print
+addi t3,t3, -1    
+bne t3,zero,sll_branch
+
+addi   a7,zero,10         # system call for exit
+#ecall                  # we are out of here.   
