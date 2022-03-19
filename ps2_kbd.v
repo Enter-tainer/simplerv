43bf66168ef36ec4c98d0ce7dd23658b100812c2
mgt <mgt@oi-wiki.org>
2022-03-19 21:11:57 +0800

    feat: new ps2 interface

--- ps2_kbd.v
+++ ps2_kbd.v
@@ -10,27 +10,20 @@ module ps2_kbd (input clk,
   reg [3:0] count;     // count ps2_data bits, internal signal, for test
   reg [9:0] buffer;     // ps2_data bits
   reg [31:0] fifo[7:0];   // data fifo
-  reg [5:0] w_ptr, r_ptr;  // fifo write and read pointers
-  reg [2:0] ps2_clk_sync;
+  reg [4:0] w_ptr, r_ptr;  // fifo write and read pointers
   
-  always @ (posedge clk) begin
-    ps2_clk_sync <= {ps2_clk_sync[1:0],ps2_clk};
-  end
-  wire sampling = ps2_clk_sync[2] & ~ps2_clk_sync[1];
-  always @ (posedge clk) begin
+  always @ (negedge ps2_clk) begin
     if (rst == 1) begin
-      count    <= 0;
-      w_ptr    <= 0;
-      r_ptr    <= 0;
+      w_ptr <= 0;
       overflow <= 0;
-      end else if (sampling) begin
+      count    <= 0;
+    end
+    else begin
       if (count == 4'd10) begin
-        if ((buffer[0] == 0) &&  (ps2_data) &&  (~buffer[9:1])) begin
-          fifo[w_ptr] <= buffer[8:1];   // kbd scan code
-          overflow <= overflow |  (r_ptr == (w_ptr + 1));
-          if (r_ptr != (w_ptr + 1)) begin
-            w_ptr <= w_ptr + 1;
-          end
+        fifo[w_ptr] <= buffer[8:1];   // kbd scan code
+        overflow <= overflow |  (r_ptr == (w_ptr + 1));
+        if (r_ptr != (w_ptr + 1)) begin
+          w_ptr <= w_ptr + 1;
         end
         count <= 0; // for next
       end
@@ -39,10 +32,13 @@ module ps2_kbd (input clk,
         count         <= count + 1'b1;   // count ps2_data bits
       end
     end
-      if (read_enable && ready) begin
-        r_ptr    <= r_ptr + 1;
-        overflow <= 0;
-      end
+  end
+  always @(posedge clk) begin
+    if (rst == 1) begin
+      r_ptr    <= 0;
+    end else if (read_enable && ready) begin
+      r_ptr    <= r_ptr + 1;
+    end
   end
   assign ready = (w_ptr != r_ptr);
   assign data  = fifo[r_ptr];
